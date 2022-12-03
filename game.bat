308655f915d544f166f71be4cc2ea1bf8a985c67
NUGU2726 <85387156+NUGU2726@users.noreply.github.com>
2022-12-03 22:17:47 +0900

    배치파일 게임 0.1.6 버전

--- game.bat
+++ game.bat
@@ -1,7 +1,7 @@
 @echo off
 :title
 cls
-set ver=0.1.5 ver
+set ver=0.1.6 ver
 echo -----------[간단 게임]-----------
 title 미니 게임 실행기 %ver%
 echo -----------[  시작  ]-----------
@@ -134,12 +134,16 @@ title 게임 선택기
 echo 1. - 업/다운
 echo 2. - 기억력테스트
 echo 3. - 간단 RPG
-echo 4. - 취소(메뉴로 이동하기)
-set /p out=선택하세요(1,2,3)
+echo 4. - 음원다운로드 (파이썬 3 + pip 필요)
+echo 5. - 가위바위보
+echo 6. - 취소(메뉴로 이동하기)
+set /p out=선택하세요(1,2,3,4,5)
 if %out%==1 goto updown
 if %out%==2 goto memory
 if %out%==3 goto sorry
-if %out%==4 goto title
+if %out%==4 goto downStart
+if %out%==5 goto rcpstart
+if %out%==6 goto title
 
 :memorys
 set score=0
@@ -222,12 +226,143 @@ echo 제작자
 echo - nugu2726
 timeout /t 2 /nobreak >nul
 echo 버전
-echo - ver 0.1.5
+echo - ver 0.1.6
 timeout /t 2 /nobreak >nul
 echo 제작환경
 echo - 그냥 cmd
+timeout /t 2 /nobreak >nul
+echo GitHub
+echo - https://github.com/NUGU2726/.Bat-Games
 timeout /t 2 /nobreak >nul
 echo 끝. 계속할려면 아무키나 누르세요.
 pause >nul
 cls
 goto title
+
+REM Windows 환경에서 Python 3와 pip 가 설치된 조건 하에 자동으로
+REM 스크립트를 실행할 수 있도록 환경을 만들어주는 배치 파일입니다.
+REM 배치 파일 안에 파이썬 스크립트가 있기 때문에 이 파일만 실행하면
+REM 자동으로 .py 파일이 생성됩니다.
+
+:downStart
+echo 스크립트 로드 중...
+chcp 65001 >nul
+echo import requests, pafy, webbrowser >ytga.py
+echo from argparse import ArgumentParser >>ytga.py
+echo def geturl(val): >>ytga.py
+echo     s="i.ytimg.com/vi/"  >>ytga.py
+echo     return val[val.find(s)+len(s):val.find(s)+len(s)+11] >>ytga.py
+echo parser = ArgumentParser() >>ytga.py
+echo parser.add_argument("-q", "--query") >>ytga.py
+echo args = parser.parse_args() >>ytga.py
+echo title = args.query >>ytga.py
+echo r = requests.get("https://www.youtube.com/results?search_query="+title+' "topic"').text >>ytga.py
+echo video = pafy.new(geturl(r)) >>ytga.py
+echo url = video.getbestaudio().url >>ytga.py
+echo print('\n=== Search Result ===\n'+str(video)) >>ytga.py
+echo webbrowser.open(url) >>ytga.py
+chcp 949 > nul
+
+echo 파이썬 설치 확인 중...
+python --version | find "Python 3"
+if %ERRORLEVEL% == 1 goto nopy
+echo pip 설치 확인 중...
+pip --version >nul
+if %ERRORLEVEL% == 9009 goto nopip
+echo 파이썬 모듈 설치 확인중...
+pip list | find "requests"
+if %ERRORLEVEL% == 1 goto inst
+pip list | find "youtube-dl"
+if %ERRORLEVEL% == 1 goto inst
+pip list | find "pafy"
+if %ERRORLEVEL% == 1 goto inst
+
+goto down
+
+:inst
+echo 필요한 패키지가 설치되어 있지 않습니다. 설치를 진행합니다.
+pip install requests youtube_dl pafy
+goto main
+
+:nopy
+echo 파이썬이 설치되어 있지 않은 것 같습니다. 설치 후 다시 실행해 주세요.
+pause
+exit
+
+:nopip
+echo pip가 설치되어 있지 않은 것 같습니다. 설치 후 다시 실행해 주세요.
+pause
+exit
+
+:down
+cls
+color 0f
+set /p que="검색 쿼리 입력 (아티스트, 곡 제목, ...): "
+ytga.py -q "%que%"
+pause
+
+:rcpstart
+echo 1 가위
+echo 2 바위
+echo 3 보
+set /p hand=선택:
+ 
+set /a comhand=%RANDOM% %%3 + 1
+ 
+echo 컴퓨터값 : %comhand%
+ 
+if %comhand% EQU 1 echo 컴퓨터 : 가위
+if %comhand% EQU 2 echo 컴퓨터 : 바위
+if %comhand% EQU 3 echo 컴퓨터 : 보
+ 
+ 
+if %hand% EQU %comhand% goto SAME
+ 
+if %hand% EQU 1 (
+    if %comhand% EQU 2 (
+        goto LOSE
+    )
+    if %comhand% EQU 3 (
+        goto WIN
+    )
+)
+ 
+if %hand% EQU 2 (
+    if %comhand% EQU 1 (
+        goto WIN
+    )
+    if %comhand% EQU 3 (
+        goto LOSE
+    )
+)
+ 
+if %hand% EQU 3 (
+    if %comhand% EQU 1 (
+        goto LOSE
+    )
+    if %comhand% EQU 2 (
+        goto WIN
+    )
+)
+ 
+cls
+echo 1-3 중 하나를 입력
+goto start
+ 
+:SAME
+echo 비김
+goto end
+ 
+:WIN
+echo 당신의 승리
+goto end
+ 
+:LOSE
+echo 당신의 패배
+goto end
+ 
+:end
+pause >nul
+ 
+cls
+goto rcpstart
